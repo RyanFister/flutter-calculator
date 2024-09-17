11f3e7d76d46562f38630f11c74f289c235563bb
Ryan F <ryan_fister@uri.edu>
2024-09-16 20:31:01 -0400

    Initial commit of the Flutter calculator app

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GitHub Copilot Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _expression = '';
+  String _result = '';
+
+  void _onPressed(String value) {
+    setState(() {
+      if (value == 'C') {
+        _expression = '';
+        _result = '';
+      } else if (value == '=') {
+        try {
+          final expression = Expression.parse(_expression);
+          final evaluator = const ExpressionEvaluator();
+          final result = evaluator.eval(expression, {});
+          _result = ' = $result';
+        } catch (e) {
+          _result = ' Error';
+        }
+      } else {
+        _expression += value;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final fontSize = screenWidth * 0.05;
+    final buttonSize = screenWidth * 0.2;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GitHub Copilot Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                '$_expression$_result',
+                style: TextStyle(fontSize: fontSize),
+              ),
+            ),
+          ),
+          Container(
+            height: screenHeight * 0.6,
+            child: GridView.count(
+              crossAxisCount: 4,
+              children: [
+                '7', '8', '9', '/',
+                '4', '5', '6', '*',
+                '1', '2', '3', '-',
+                'C', '0', '=', '+'
+              ].map((value) {
+                return GestureDetector(
+                  onTap: () => _onPressed(value),
+                  child: Container(
+                    margin: EdgeInsets.all(4.0),
+                    width: buttonSize,
+                    height: buttonSize,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        value,
+                        style: TextStyle(fontSize: fontSize, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
